6f33d0d33f71a31f883159485cbf1a4951d45dd1
Matthew Guthaus <mrg@ucsc.edu>
2026-03-01 14:08:04 -0800

    Remove M8/M9 since they don't have valid extraction data.

--- designs/asap7/gemmini/pdn.tcl
+++ designs/asap7/gemmini/pdn.tcl
@@ -14,20 +14,16 @@ set_voltage_domain -name {CORE} -power {VDD} -ground {VSS}
 ####################################
 # standard cell grid
 ####################################
-define_pdn_grid -name {top} -voltage_domains {CORE} -pins {M9}
+define_pdn_grid -name {top} -voltage_domains {CORE} -pins {M7}
 add_pdn_stripe -grid {top} -layer {M1} -width {0.018} -pitch {0.54} -offset {0} -followpins
 add_pdn_stripe -grid {top} -layer {M2} -width {0.018} -pitch {0.54} -offset {0} -followpins
 add_pdn_stripe -grid {top} -layer {M5} -width {0.12}  -spacing {0.072} -pitch {2.7} -offset {0.300}
 add_pdn_stripe -grid {top} -layer {M6} -width {0.288} -spacing {0.096} -pitch {2.7} -offset {0.513}
 add_pdn_stripe -grid {top} -layer {M7} -width {0.416} -spacing {0.10}  -pitch {4.0} -offset {0.500}
-add_pdn_stripe -grid {top} -layer {M8} -width {0.40}  -spacing {0.10}  -pitch {4.0} -offset {0.500}
-add_pdn_stripe -grid {top} -layer {M9} -width {0.80}  -spacing {0.20}  -pitch {8.0} -offset {1.000}
 add_pdn_connect -grid {top} -layers {M1 M2}
 add_pdn_connect -grid {top} -layers {M2 M5}
 add_pdn_connect -grid {top} -layers {M5 M6}
 add_pdn_connect -grid {top} -layers {M6 M7}
-add_pdn_connect -grid {top} -layers {M7 M8}
-add_pdn_connect -grid {top} -layers {M8 M9}
 ####################################
 # macro grids
 ####################################
